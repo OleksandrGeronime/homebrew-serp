1dbed6acaa97ea2a183949d22b5199d7aa344d9d
SERP CI <ci@serp.dev>
2026-07-04 05:39:12 +0000

    release v0.6.2: update formulas

--- Formula/serp-dev.rb
+++ Formula/serp-dev.rb
@@ -1,13 +1,13 @@
 class SerpDev < Formula
   desc "SERP development files and code generator"
   homepage "https://github.com/OleksandrGeronime/serp"
-  version "0.5.2"
+  version "0.6.2"
   license "Proprietary"
 
   on_macos do
     on_arm do
       url "https://github.com/OleksandrGeronime/serp/releases/download/v#{version}/serp-dev-#{version}-arm64_sequoia.tar.gz"
-      sha256 "ce5ec2dc3b23e08cb2b2ebf5ca01c84b6bcd5d3fdaaac799089832b548a0101c"
+      sha256 "030edd3fc04e7d089b0832fc3ada48a66950efe577c4ca30dacac9bd25b1efbe"
     end
     on_intel do
       url "https://github.com/OleksandrGeronime/serp/releases/download/v#{version}/serp-dev-#{version}-x86_64_ventura.tar.gz"
--- Formula/serp.rb
+++ Formula/serp.rb
@@ -1,13 +1,13 @@
 class Serp < Formula
   desc "SERP runtime libraries"
   homepage "https://github.com/OleksandrGeronime/serp"
-  version "0.5.2"
+  version "0.6.2"
   license "Proprietary"
 
   on_macos do
     on_arm do
       url "https://github.com/OleksandrGeronime/serp/releases/download/v#{version}/serp-#{version}-arm64_sequoia.tar.gz"
-      sha256 "e6b470e286de01b416c7aa105335b22fbe2ee226eec8d2ff34a30708a080823f"
+      sha256 "72a386cc225a6bf5e3fafba3023a8f98205a193581d7662304f730b6a93f0722"
     end
     on_intel do
       url "https://github.com/OleksandrGeronime/serp/releases/download/v#{version}/serp-#{version}-x86_64_ventura.tar.gz"
